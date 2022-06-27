d14295cf874fe97fe8a6c5ef4b2e7e12e83fa519
SeRgNeschetniy <xdsergx@gmail.com>
2022-06-27 14:05:53 +0300

    add home work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = ({ preview, original, description }) => {
+  return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+          </div>`;
+};
+
+gallery.innerHTML = galleryItems.map(galleryItem).join("");
+
+gallery.addEventListener("click", onOpenImage);
+
+function onOpenImage(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", (event) => {
+          if (event.key === "Escape") {
+            instance.close();
+          }
+        });
+      },
+    }
+  );
+
+  instance.show();
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = ({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>`;
+};
+
+gallery.innerHTML = galleryItems.map(galleryItem).join("");
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  captions: true,
+});
 
 console.log(galleryItems);
